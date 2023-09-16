039b1d69f89284c57c31802f8d219f1d10074904
ashishkhadye33 <ashishkhadye33@gmail.com>
2023-09-16 10:55:52 +0530

    Add project files.

---
+++ Emp/Employee.Master.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Emp
+{
+    public partial class Employee : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["EmployeeId"] != null)
+            {
+                liregister.Style.Add("display","none");
+                lilogin.Style.Add("display", "none");
+                lilogout.Style.Add("display", "block");
+            }
+            else
+            {
+                liregister.Style.Add("display", "block");
+                lilogin.Style.Add("display", "block");
+                lilogout.Style.Add("display", "none");
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.RemoveAll();
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
---
+++ Emp/Register.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Emp
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("insert into EmpRegister values (@name,@email,@contact,@dept,@password)",con);
+            cmd.Parameters.AddWithValue("@name",txtName.Text);
+            cmd.Parameters.AddWithValue("@email",txtEmail.Text);
+            cmd.Parameters.AddWithValue("@contact",txtContact.Text);
+            cmd.Parameters.AddWithValue("@dept",ddldept.SelectedItem.Text);
+            cmd.Parameters.AddWithValue("@password",txtPassword.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            txtName.Text = "";
+            txtEmail.Text = "";
+            txtPassword.Text = "";
+            txtContact.Text = "";
+            ddldept.SelectedIndex= 0;
+            this.ClientScript.RegisterStartupScript(this.GetType(), "SweetAlert", "swal('Registered successfully..!','you can log-in now','success');", true);
+
+        }
+    }
+}
